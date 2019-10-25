76f6907e6dd711813ec30dbbb7e26ef73df955d2
David Levine <55397183+dlevine300@users.noreply.github.com>
2019-10-25 18:18:19 -0400

    Delete perimeter.js

--- perimeter.js
+++
@@ -1,8 +0,0 @@
-const ReadlineSync =require('readline-Sync');
-const Width =ReadlineSync.question('\nWidth: ');
-const Length =ReadlineSync.question('Length: ');
-const Answer =(2 * Length + 2 * Width)
-const Conversion_l =Length * 2.54
-const Conversion_w =Width * 2.54
-const Perimeter =2*(Conversion_l + Conversion_w).toLocaleString('en', { minimunFractionDigits: 2, maximumFractionDigits: 2})
-console.log('\nA(n) '+ Width + '-by-' + Length + '-inch sheet paper has an area of ' + Perimeter + ' centimeter(s).')
